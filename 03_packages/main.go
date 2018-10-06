f6cced0b71d1e01359161e85df458db419993625
raynaldo <raynaldo@mii.co.id>
2018-10-07 00:47:48 +0700

    functions

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
-	"strutil"
 
-	"github.com/raaynaldo/LearnGoLang/src/03_packages/strutil"
+	"github.com/LearnGoLang/03_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(16))
-	fmt.Prinln(strutil.Reverse("Hello"))
+	fmt.Println(strutil.Reverse("Hello"))
 }
