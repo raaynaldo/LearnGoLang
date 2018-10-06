8d494c9f6ac1bff29f9ef3cb7b05d38bb3056a2d
raynaldo <raynaldo@mii.co.id>
2018-10-07 00:39:19 +0700

    Re-Init

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"math"
+	"strutil"
+
+	"github.com/raaynaldo/LearnGoLang/src/03_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(16))
+	fmt.Prinln(strutil.Reverse("Hello"))
 }
